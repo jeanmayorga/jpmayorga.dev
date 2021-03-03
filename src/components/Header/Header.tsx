1700c74292013e97061968f7a6145ab6372bb018
Paul Mayorga <jpmayorga@outlook.com>
2021-03-03 15:47:43 -0500

    start(project): first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className='header z-40 w-full fixed py-3 bg-white bg-opacity-60'>
+      <div className='container mx-auto'>Hola mundoo</div>
+    </header>
+  );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { faApple } from '@fortawesome/free-brands-svg-icons';
+import { faLaptopCode, faRocket } from '@fortawesome/free-solid-svg-icons';
+import { AppProps } from 'next/app';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+library.add(faApple, faLaptopCode, faRocket);
+
+import '../styles/global.css';
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
